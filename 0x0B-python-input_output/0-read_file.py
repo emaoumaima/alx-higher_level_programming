ac327be1fd13881cbebaeaec22477bcc7be0a885
emaoumaima <sellouane.oumaima@gmail.com>
2023-10-02 07:03:41 -0700

    update files

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module reads from a file """
+
+
+def read_file(filename=""):
+    """ Function reads file
+
+    Args:
+        filename: filename
+
+    Raises
+        Exception: can file be opened
+
+    """
+
+    with open(filename, 'r', encoding="utf-8") as f:
+        r = f.read()
+        print(r, end='')
