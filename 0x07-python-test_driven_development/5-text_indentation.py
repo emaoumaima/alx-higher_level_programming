fcbf8da8c17a30fcd52eef1adf4532ece17ab0bb
Oumaima SLL <58479472+emaoumaima@users.noreply.github.com>
2023-11-02 21:25:37 +0100

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,25 +1,30 @@
 #!/usr/bin/python3
+# 5-text_indentation.py
+"""Defines a text-indentation function."""
+
+
 def text_indentation(text):
-    """
-    prints a text with 2 new lines after each of these
-    characters: ., ? and :
+    """Print text with two new lines after each '.', '?', and ':'.
 
     Args:
-                text (str): The text string to be parsed.
-
+        text (string): The text to print.
     Raises:
-                TypeError: If text is not a string.
+        TypeError: If text is not a string.
     """
-    if type(text) is not str:
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    s = text[:]
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
 
-    for delimeter in ".?:":
-        l_text = s.split(delimeter)
-        s = ""
-        for i in l_text:
-            i = i.strip(" ")
-            s = i + delimeter if s is "" else s + "\n\n" + i + delimeter
-
-    print(s[:-3], end="")
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
