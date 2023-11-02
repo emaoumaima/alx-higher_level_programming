31390c081ce3a2459388bf7c8c94c47d173285b6
Oumaima SLL <58479472+emaoumaima@users.noreply.github.com>
2023-11-02 20:57:15 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
+"""Print first and last name"""
+
+
 def say_my_name(first_name, last_name=""):
-    """
-    Prints "My name is <first name> <last name>"
-
+    """Func that print name
     Args:
-                first_name: firstname
-        last_name: lastname
-
-        Raises:
-        TypeError: If first_name or last_name are not strings.
-
-    Returns:
-                nothing
-
-    """
+    first_name (str)
+    last_name (str) """
     if not isinstance(first_name, str):
-        raise TypeError('first_name must be a string')
+        raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
-        raise TypeError('last_name must be a string')
-    print("My name is {}{}".format(first_name, " " +
-          last_name if last_name else ""))
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
