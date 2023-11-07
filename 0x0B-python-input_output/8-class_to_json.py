4bbc1ec18a156cba77ceb89b8600944e7ae3cf78
emaoumaima <sellouane.oumaima@gmail.com>
2023-11-07 12:12:12 -0800

    delete

--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# 8-class_to_json.py
-# Juan Duque <3428@holbertonschool.com>
-""" Declare the new function return new dict to json"""
-
-
-def class_to_json(obj):
-    """ Return obj in dict mode"""
-    return obj.__dict__
--- 0x0B-python-input_output/tests/101-stats.py
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/python3
-# 101-stats.py
